1083efdf7df8335ca9494b579a75ced00c4f9e7a
Abdulkader <abdalo01@easv365.dk>
2023-04-26 19:01:57 +0200

    added the logic for the handleCreatDocument to switch scenes.

--- src/main/java/dk/easv/gui/controllers/AdminViewController.java
+++ src/main/java/dk/easv/gui/controllers/AdminViewController.java
@@ -1,11 +1,16 @@
 package dk.easv.gui.controllers;
 
+import dk.easv.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,8 +34,12 @@ public class AdminViewController implements Initializable {
 
     }
     @FXML
-    private void handleCreateDocument() {
-
+    private void handleCreateDocument() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/create-document.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 990, 625);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
     @FXML
     private void handleEditDocument() {

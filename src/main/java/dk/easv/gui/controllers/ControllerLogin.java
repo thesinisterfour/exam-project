b1a5af189abd5d0199f58c674288a724f56569e9
Abdulkader <abdalo01@easv365.dk>
2023-04-26 18:56:31 +0200

    Added the logic for the loginButton

--- src/main/java/dk/easv/gui/controllers/ControllerLogin.java
+++ src/main/java/dk/easv/gui/controllers/ControllerLogin.java
@@ -1,8 +1,15 @@
 package dk.easv.gui.controllers;
 
 
+import dk.easv.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class ControllerLogin {
@@ -11,8 +18,11 @@ public class ControllerLogin {
     @FXML
     private TextField password;
     @FXML
-    public void loginButton() {
-
+    public void loginButton() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/admin-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 760, 480);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
-
 }

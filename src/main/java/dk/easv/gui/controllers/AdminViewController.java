cf629c9dd88aa46aa84489a01b6cb00b38eae03c
Abdulkader <abdalo01@easv365.dk>
2023-04-26 17:54:50 +0200

    Finished the admin-View.fxml and the AdminViewController

--- src/main/java/dk/easv/gui/controllers/AdminViewController.java
+++ src/main/java/dk/easv/gui/controllers/AdminViewController.java
@@ -1,11 +1,49 @@
 package dk.easv.gui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-public class AdminViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminViewController implements Initializable {
 
     @FXML
-    private void handleLogoutButton(){
+    private TextField userName;
+    @FXML
+    private ComboBox comboBox;
+    @FXML
+    private TableView tableView;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+    @FXML
+    private void handleLogout() {
 
     }
+    @FXML
+    private void handleDelete() {
+
+    }
+    @FXML
+    private void handleCreateDocument() {
+
+    }
+    @FXML
+    private void handleEditDocument() {
+
+    }
+    @FXML
+    private void handleUsers() {
+
+    }
+    @FXML
+    private void handleCustomers() {
+
+    }
+
+
 }

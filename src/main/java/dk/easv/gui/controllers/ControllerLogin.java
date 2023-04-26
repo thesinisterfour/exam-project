e19d9af67d9dd3074e36bf952faf84f368f5c00b
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-04-26 21:45:08 +0200

    add factory for generating new view, make login-view responsive

--- src/main/java/dk/easv/gui/controllers/ControllerLogin.java
+++ src/main/java/dk/easv/gui/controllers/ControllerLogin.java
@@ -1,28 +1,25 @@
 package dk.easv.gui.controllers;
 
 
-import dk.easv.Main;
+import dk.easv.gui.rootContoller.RootController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 
-public class ControllerLogin {
+public class ControllerLogin extends RootController {
     @FXML
     private TextField username;
     @FXML
     private TextField password;
     @FXML
-    public void loginButton() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/admin-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 760, 480);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
+    private void loginButtonAction(ActionEvent actionEvent) {
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/admin-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 760, 480);
+//        Stage stage = new Stage();
+//        stage.setScene(scene);
+//        stage.show();
+
+        System.out.println("Logged in");
     }
 }

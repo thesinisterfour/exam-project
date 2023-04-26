698d3115fbb1e2bc912eed263d18808468f7c3b7
Nicola Martinez <nicoenrique_1994@hotmail.com>
2023-04-26 23:34:37 +0200

    changed by error rolledback

--- src/main/java/dk/easv/Main.java
+++ src/main/java/dk/easv/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         if (Files.exists(Config.CONFIG_PATH.getUrl())){
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/worker-users-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/hello-view.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             stage.setTitle("Hello!");
             stage.setScene(scene);

791a119c39fd8893fba500b18d22cd766d49ec40
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-04-26 10:17:50 +0200

    add check for existing config

--- src/main/java/dk/easv/Main.java
+++ src/main/java/dk/easv/Main.java
@@ -8,17 +8,40 @@ import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Optional;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        if (!Helpers.checkConfig()){
+        if (Files.exists(Helpers.CONFIG_PATH.getUrl())){
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/hello-view.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 320, 240);
             stage.setTitle("Hello!");
             stage.setScene(scene);
             stage.show();
+        } else {
+            showConfigNotFoundAlert();
+        }
+    }
+
+    private static void showConfigNotFoundAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Config file not found");
+        alert.setContentText("""
+                Config file not found
+                Please create a config file in the following path:
+                src/main/resources/dk/easv/config.cfg
+                """);
+        Optional<ButtonType> resultButton = alert.showAndWait();
+        if (resultButton.isPresent()) {
+            if (resultButton.get() == ButtonType.OK) {
+                System.exit(0);
+            }
+        } else {
+
+            System.exit(1);
         }
     }
 

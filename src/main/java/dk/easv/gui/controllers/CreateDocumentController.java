fbece77637da8bce7fc23c9d6d78e03b0b5fce7f
Abdulkader <abdalo01@easv365.dk>
2023-04-26 19:29:15 +0200

    added FXMXL to creating a document and the controller.

---
+++ src/main/java/dk/easv/gui/controllers/CreateDocumentController.java
@@ -0,0 +1,29 @@
+package dk.easv.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class CreateDocumentController {
+    @FXML
+    private TextField txfDocumentName;
+    @FXML
+    private TextField txfLocation;
+    @FXML
+    private TextField txfDescription;
+    @FXML
+    private ComboBox comboBox;
+    @FXML
+    private DatePicker dpStartDate;
+
+
+    @FXML
+    private void handleSave() {
+
+    }
+    @FXML
+    private void handleCancel() {
+
+    }
+}

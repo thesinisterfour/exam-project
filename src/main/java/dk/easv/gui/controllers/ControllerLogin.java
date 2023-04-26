3c23f3cce06c047a7691e764d9e0d3c0a7bd29b0
LeeVoi <Abual3a7ed.6@gmail.com>
2023-04-26 12:55:00 +0200

    created the login-screen.fxml and the ControllerLogin.java

---
+++ src/main/java/dk/easv/gui/controllers/ControllerLogin.java
@@ -0,0 +1,18 @@
+package dk.easv.gui.controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+public class ControllerLogin {
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+    @FXML
+    public void loginButton() {
+
+    }
+
+}

6abdbc2607c80c4727476e55408eba0b68f56ba4
jklaric <klaric9jakov@gmail.com>
2023-04-27 11:41:15 +0200

    Create displaying logic for ProjectManagerViewController

--- src/main/java/dk/easv/gui/controllers/ControllerLogin.java
+++ src/main/java/dk/easv/gui/controllers/ControllerLogin.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,13 +17,12 @@ public class ControllerLogin extends RootController {
     @FXML
     private TextField password;
     @FXML
-    private void loginButtonAction(ActionEvent actionEvent) {
+    private void loginButtonAction(ActionEvent actionEvent) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/admin-view.fxml"));
 //        Scene scene = new Scene(fxmlLoader.load(), 760, 480);
 //        Stage stage = new Stage();
 //        stage.setScene(scene);
 //        stage.show();
-
         System.out.println("Logged in");
     }
 

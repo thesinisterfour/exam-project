df57eabe49f4d0e271c407c556952433ae2970d4
jklaric <126068242+danielgam3@users.noreply.github.com>
2023-04-27 16:31:00 +0200

    Create login logic
    Problem with loading fxml files

--- src/main/java/dk/easv/bll/CRUDLogic.java
+++ src/main/java/dk/easv/bll/CRUDLogic.java
@@ -8,10 +8,17 @@ import dk.easv.helpers.DAOType;
 
 import java.sql.SQLException;
 import java.util.concurrent.ConcurrentMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class CRUDLogic {
+    private ICRUDDao<User> userDAO = CRUDDAOFactory.getDao(DAOType.USER_DAO);
+    private User user;
+
     public int addUser(User user) throws SQLException, NullPointerException {
-        ICRUDDao<User> userDAO = CRUDDAOFactory.getDao(DAOType.USER_DAO);
         if (userDAO == null) {
             throw new NullPointerException("UserDAO is null");
         } else {
@@ -27,7 +34,15 @@ public class CRUDLogic {
         }
 
 
-
+    public User checkForUser(String username, String password) throws SQLException {
+        ConcurrentMap<Integer, User> userMap = userDAO.getAll();
+        List<Map.Entry<Integer, User>> userList = new ArrayList<>(userMap.entrySet());
+        for (Map.Entry<Integer, User> selectedUser:userList) {
+            if (selectedUser.getValue().getUsername().equals(username) && selectedUser.getValue().getPassword().equals(password)){
+                return selectedUser.getValue();
+            }
+        }
+        return null;
     }
 
     public ConcurrentMap<Integer, Customer> getAllCustomers() throws SQLException{

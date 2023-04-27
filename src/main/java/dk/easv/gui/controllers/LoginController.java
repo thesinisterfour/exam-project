96a7f57791f64ab25d917a9d0ca571a5ff75261a
LeeVoi <Abual3a7ed.6@gmail.com>
2023-04-27 10:45:35 +0200

    changed the controller name from ControllerLogin to LoginController.

--- src/main/java/dk/easv/gui/controllers/LoginController.java
+++ src/main/java/dk/easv/gui/controllers/LoginController.java
@@ -1,18 +1,10 @@
 package dk.easv.gui.controllers;
 
 
-import dk.easv.gui.controllerFactory.ControllerFactory;
 import dk.easv.gui.rootContoller.RootController;
-import dk.easv.helpers.ViewType;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 
 public class LoginController extends RootController {
@@ -20,27 +12,14 @@ public class LoginController extends RootController {
     private MFXTextField username;
     @FXML
     private MFXTextField password;
-
-    private Stage stage;
-
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }
     @FXML
-    private void loginButtonAction(ActionEvent actionEvent) throws IOException {
-        if (stage == null){
-            this.stage = this.getStage();
-        }
-        RootController controller = ControllerFactory.loadFxmlFile(ViewType.ADMIN);
-        stage.setScene(new Scene(controller.getView(), 760, 480));
-        stage.setTitle("Admin");
+    private void loginButtonAction(ActionEvent actionEvent) {
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/admin-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 760, 480);
+//        Stage stage = new Stage();
+//        stage.setScene(scene);
+//        stage.show();
 
-//        System.out.println("Logged in");
-    }
-
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
+        System.out.println("Logged in");
     }
 }

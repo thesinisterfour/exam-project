29b15a68f630b91f510f12f00f8482c62f86c68a
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-05-01 11:30:48 +0200

    add document dao to factory

--- src/main/java/dk/easv/dal/CRUDDAOFactory.java
+++ src/main/java/dk/easv/dal/CRUDDAOFactory.java
@@ -1,23 +1,17 @@
 package dk.easv.dal;
 
 import dk.easv.dal.dao.CustomerDAO;
+import dk.easv.dal.dao.DocumentDAO;
 import dk.easv.dal.dao.UserDAO;
 import dk.easv.dal.interafaces.ICRUDDao;
 import dk.easv.helpers.DAOType;
 
 public class CRUDDAOFactory {
     public static ICRUDDao getDao(DAOType dao) {
-        switch (dao) {
-            case CUSTOMER_DAO:
-                return new CustomerDAO();
-            case EVENT_DAO:
-//                return new EventDAO();
-            case TICKET_DAO:
-//                return new TicketDAO();
-            case USER_DAO:
-                return new UserDAO();
-            default:
-                return null;
-        }
+        return switch (dao) {
+            case CUSTOMER_DAO -> new CustomerDAO();
+            case USER_DAO -> new UserDAO();
+            case DOCUMENT_DAO -> new DocumentDAO();
+        };
     }
 }
--- src/main/java/dk/easv/helpers/DAOType.java
+++ src/main/java/dk/easv/helpers/DAOType.java
@@ -2,7 +2,6 @@ package dk.easv.helpers;
 
 public enum DAOType {
     CUSTOMER_DAO,
-    EVENT_DAO,
-    TICKET_DAO,
-    USER_DAO;
+    USER_DAO,
+    DOCUMENT_DAO
 }

506f08b3adcbe9476d96b27851fb589f9c96add8
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-05-25 19:49:25 +0200

    add comments to content methods

--- src/main/java/dk/easv/gui/controllers/DocumentViewController.java
+++ src/main/java/dk/easv/gui/controllers/DocumentViewController.java
@@ -9,8 +9,8 @@ import dk.easv.gui.models.ContentModel;
 import dk.easv.gui.models.interfaces.IContentModel;
 import dk.easv.gui.models.tasks.RetrieveContentTask;
 import dk.easv.gui.rootContoller.RootController;
-import dk.easv.helpers.CustomerType;
 import dk.easv.helpers.CurrentUser;
+import dk.easv.helpers.CustomerType;
 import dk.easv.helpers.ViewType;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXScrollPane;
@@ -44,7 +44,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
-import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The DocumentViewController class is a JavaFX controller that handles adding and saving text and
@@ -52,10 +55,7 @@ import java.util.concurrent.*;
  * database.
  */
 public class DocumentViewController extends RootController {
-
-    private final ScheduledExecutorService scheduledSaveService = Executors.newSingleThreadScheduledExecutor();
     private final int scaleOffset = 50;
-    private final Label emptyLabel = new Label("No content to display");
     private final IContentModel model = ContentModel.getInstance();
     private final VBox dropImage = createDropImageVBox();
     @FXML
@@ -95,7 +95,7 @@ public class DocumentViewController extends RootController {
         scaleReferencePane = vbox;
 
         CurrentUser actualUser = CurrentUser.getInstance();
-        if (actualUser.getRole() == Role.SALESPERSON){
+        if (actualUser.getRole() == Role.SALESPERSON) {
             controlsHbox.setVisible(false);
             saveButton.setDisable(true);
         }
@@ -118,65 +118,9 @@ public class DocumentViewController extends RootController {
             HBox hBox = addImage(image);
             children.add(hBox);
             new FadeIn(hBox).play();
-
-        }
-
-//        emptyCheck();
-    }
-
-    private VBox createSideButtonsVBox() {
-        MFXButton buttonUp = new MFXButton("↑");
-        buttonUp.setOnAction(this::moveUp);
-        MFXButton buttonDelete = new MFXButton("❌");
-        buttonDelete.setOnAction(this::deleteNode);
-        MFXButton buttonDown = new MFXButton("↓");
-        buttonDown.setOnAction(this::moveDown);
-        VBox vBox = new VBox(8, buttonUp, buttonDelete, buttonDown);
-        vBox.setAlignment(Pos.CENTER);
-        return vBox;
-    }
-
-    private void moveUp(ActionEvent event) {
-        ObservableList<Node> children = vbox.getChildren();
-        MFXButton button = (MFXButton) event.getSource();
-        HBox hBox = (HBox) button.getParent().getParent();
-        int i = children.indexOf(hBox);
-        if (i != 0) {
-            Node swapping = children.remove(i);
-            Node swapFor = children.remove(i - 1);
-            children.add(i - 1, swapping);
-            children.add(i, swapFor);
         }
     }
 
-    private void moveDown(ActionEvent event) {
-        ObservableList<Node> children = vbox.getChildren();
-        MFXButton button = (MFXButton) event.getSource();
-        HBox hBox = (HBox) button.getParent().getParent();
-        int i = children.indexOf(hBox);
-        if (i != children.size() - 1) {
-            Node swapFor = children.remove(i + 1);
-            Node swapping = children.remove(i);
-            children.add(i, swapFor);
-            children.add(i + 1, swapping);
-        }
-    }
-
-    private void deleteNode(ActionEvent event) {
-        ObservableList<Node> children = vbox.getChildren();
-        MFXButton button = (MFXButton) event.getSource();
-        HBox hBox = (HBox) button.getParent().getParent();
-        if (hBox.getId() != null) {
-            int id = Integer.parseInt(hBox.getId());
-            try {
-                model.deleteMap(id);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        children.remove(hBox);
-//        emptyCheck();
-    }
 
     /**
      * This function adds a new MFXTextField to a VBox container when an action event occurs.
@@ -207,15 +151,20 @@ public class DocumentViewController extends RootController {
     private void saveContent() {
         ObservableList<Node> children = vbox.getChildren();
         for (int i = 0; i < children.size(); i++) {
+            // Check if the node is an HBox
             if (!(children.get(i) instanceof HBox hBox)) {
-                return;
+                break;
             }
+            // Check if the HBox contains an MFXTextField or an ImageView
             if (hBox.getChildren().get(0) instanceof MFXTextField mfxTextField) {
                 try {
+                    // Check if the HBox has an ID
                     String id = hBox.getId();
                     if (id == null) {
+                        // if it does not call method that adds a new text to the database
                         model.addText(i, mfxTextField.getText());
                     } else {
+                        // if it does call method that updates the text in the database
                         model.addText(Integer.parseInt(id), i, mfxTextField.getText());
                     }
                 } catch (SQLException e) {
@@ -223,10 +172,13 @@ public class DocumentViewController extends RootController {
                 }
             } else if (hBox.getChildren().get(0) instanceof ImageView imageView) {
                 try {
+                    // Check if the HBox has an ID
                     String id = hBox.getId();
                     if (id == null) {
+                        // if it does not call method that adds a new image to the database
                         model.addImage(i, imageView.getImage());
                     } else {
+                        // if it does call method that updates the index of the image in the database
                         model.addImage(Integer.parseInt(id), i);
                     }
                 } catch (SQLException e) {
@@ -255,101 +207,6 @@ public class DocumentViewController extends RootController {
         }
     }
 
-    /**
-     * This function populates a JavaFX VBox with content retrieved from a ConcurrentSkipListMap using
-     * RetrieveContentTask and updates the VBox with either an ImageView or MFXTextField depending on
-     * the type of content retrieved.
-     */
-    private void populateContent() {
-        //TODO: refactoring on this
-        ObservableList<Node> children = vbox.getChildren();
-        children.add(new ImageView(Objects.requireNonNull(Main.class.getResource("icons/loading.gif")).toString()));
-
-        Platform.runLater(() -> {
-            ExecutorService executorService = Executors.newSingleThreadExecutor();
-            Task<ConcurrentNavigableMap<Integer, Integer>> mapTask = new Task<>() {
-                @Override
-                protected ConcurrentNavigableMap<Integer, Integer> call() throws Exception {
-                    return model.getContentMap();
-                }
-            };
-            mapTask.valueProperty().addListener(((obs, o, n) -> {
-                ConcurrentNavigableMap<Integer, Integer> contentMap = n;
-                ExecutorService es = Executors.newFixedThreadPool(50);
-                contentMap.forEach((k, v) -> children.add(new ImageView(Objects.requireNonNull(Main.class.getResource("icons/loading.gif")).toString())));
-                for (Integer key : contentMap.keySet()) {
-                    RetrieveContentTask task = new RetrieveContentTask(contentMap.get(key));
-                    // This code is checking if the content is an image or text and updates the VBox on change of the value property.
-                    task.valueProperty().addListener((observable, oldValue, newValue) -> Platform.runLater(() -> {
-                        Content content = (Content) newValue;
-                        if (content.getImage() != null) {
-                            HBox hBox = addImage(content.getImage());
-                            hBox.setId(String.valueOf(contentMap.get(key)));
-                            children.set(key, hBox);
-                            new FadeIn(hBox).play();
-                        } else {
-                            HBox hBox = addText(content.getText());
-                            hBox.setId(String.valueOf(contentMap.get(key)));
-                            children.set(key, hBox);
-                            new FadeIn(hBox).play();
-                        }
-                    }));
-                    es.submit(task);
-
-                }
-                es.shutdown();
-
-            }));
-            executorService.submit(mapTask);
-            executorService.shutdown();
-
-            try {
-                if (executorService.awaitTermination(30, TimeUnit.SECONDS)) {
-                    executorService.shutdownNow();
-                }
-
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            children.remove(children.size() - 1);
-        });
-
-
-    }
-
-    private HBox addText(String newValue) {
-        MFXTextField mfxTextField = new MFXTextField();
-        mfxTextField.setFloatMode(FloatMode.BORDER);
-        mfxTextField.setText(newValue);
-        mfxTextField.setMinWidth(scaleReferencePane.getWidth() - scaleOffset);
-        mfxTextField.setMaxWidth(scaleReferencePane.getWidth() - scaleOffset);
-        scaleReferencePane.widthProperty().addListener((o, oldV, newV) -> {
-            mfxTextField.setMinWidth(newV.doubleValue() - scaleOffset);
-            mfxTextField.setMaxWidth(newV.doubleValue() - scaleOffset);
-        });
-        return getHBoxWithNavButtons(mfxTextField);
-    }
-
-    private HBox addImage(Image image) {
-        ImageView imageView = new ImageView();
-
-        imageView.setImage(image);
-        imageView.preserveRatioProperty().set(true);
-        imageView.setFitWidth(scaleReferencePane.getWidth() - scaleOffset);
-        scaleReferencePane.widthProperty().addListener((o, oldV, newV) -> imageView.setFitWidth((double) newV - scaleOffset));
-        return getHBoxWithNavButtons(imageView);
-    }
-
-    @FXML
-    private void saveAsPdfPublicOnAction() {
-        saveAsPDF(CustomerType.PUBLIC);
-    }
-
-    private HBox getHBoxWithNavButtons(Node node) {
-        HBox hBox = new HBox(8, node, createSideButtonsVBox());
-        hBox.setAlignment(Pos.CENTER_LEFT);
-        return hBox;
-    }
 
     @FXML
     private void vboxOnDragDropped(DragEvent dragEvent) {
@@ -424,6 +281,11 @@ public class DocumentViewController extends RootController {
     }
 
     @FXML
+    private void saveAsPdfPublicOnAction() {
+        saveAsPDF(CustomerType.PUBLIC);
+    }
+
+    @FXML
     private void saveAsPdfPrivateOnAction(ActionEvent actionEvent) {
         saveAsPDF(CustomerType.PRIVATE);
     }
@@ -445,13 +307,166 @@ public class DocumentViewController extends RootController {
         }
     }
 
-  /*
+    private VBox createSideButtonsVBox() {
+        MFXButton buttonUp = new MFXButton("↑");
+        buttonUp.setOnAction(this::moveUp);
+        MFXButton buttonDelete = new MFXButton("❌");
+        buttonDelete.setOnAction(this::deleteNode);
+        MFXButton buttonDown = new MFXButton("↓");
+        buttonDown.setOnAction(this::moveDown);
+        VBox vBox = new VBox(8, buttonUp, buttonDelete, buttonDown);
+        vBox.setAlignment(Pos.CENTER);
+        return vBox;
+    }
 
-  Unfinished feature for auto adding drawn image
+    private void moveUp(ActionEvent event) {
+        ObservableList<Node> children = vbox.getChildren();
+        MFXButton button = (MFXButton) event.getSource();
+        HBox hBox = (HBox) button.getParent().getParent();
+        int i = children.indexOf(hBox);
+        if (i != 0) {
+            Node swapping = children.remove(i);
+            Node swapFor = children.remove(i - 1);
+            children.add(i - 1, swapping);
+            children.add(i, swapFor);
+        }
+    }
 
-        public void setDrawnImage(Image image){
-        HBox hBox = addImage(image);
-        vbox.getChildren().add(hBox);
-        new FadeIn(hBox).play();
-    }*/
+    private void moveDown(ActionEvent event) {
+        ObservableList<Node> children = vbox.getChildren();
+        MFXButton button = (MFXButton) event.getSource();
+        HBox hBox = (HBox) button.getParent().getParent();
+        int i = children.indexOf(hBox);
+        if (i != children.size() - 1) {
+            Node swapFor = children.remove(i + 1);
+            Node swapping = children.remove(i);
+            children.add(i, swapFor);
+            children.add(i + 1, swapping);
+        }
+    }
+
+    private void deleteNode(ActionEvent event) {
+        ObservableList<Node> children = vbox.getChildren();
+        MFXButton button = (MFXButton) event.getSource();
+        HBox hBox = (HBox) button.getParent().getParent();
+        if (hBox.getId() != null) {
+            int id = Integer.parseInt(hBox.getId());
+            try {
+                model.deleteContent(id);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        children.remove(hBox);
+    }
+
+    /**
+     * This function populates a JavaFX VBox with content retrieved from a ConcurrentSkipListMap using
+     * RetrieveContentTask and updates the VBox with either an ImageView or MFXTextField depending on
+     * the type of content retrieved.
+     */
+    private void populateContent() {
+        ObservableList<Node> children = vbox.getChildren();
+        //add an icon to indicate loading
+        children.add(new ImageView(Objects.requireNonNull(Main.class.getResource("icons/loading.gif")).toString()));
+
+        // run this on main thread
+        Platform.runLater(() -> {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            // new task to retrieve the content map
+            Task<ConcurrentNavigableMap<Integer, Integer>> mapTask = new Task<>() {
+                @Override
+                protected ConcurrentNavigableMap<Integer, Integer> call() throws Exception {
+                    return model.getContentMap();
+                }
+            };
+            // bind the end of task to the start of adding the content
+            mapTask.valueProperty().addListener(((obs, o, n) -> {
+                // assign the map
+                ConcurrentNavigableMap<Integer, Integer> contentMap = n;
+                ExecutorService es = Executors.newFixedThreadPool(50);
+                // add loading icons for each content
+                contentMap.forEach((k, v) -> children.add(new ImageView(Objects.requireNonNull(Main.class.getResource("icons/loading.gif")).toString())));
+                // submit a task for each content
+                for (Integer key : contentMap.keySet()) {
+                    // create the task
+                    RetrieveContentTask task = new RetrieveContentTask(contentMap.get(key));
+                    // This code is checking if the content is an image or text and updates the VBox on change of the value property.
+                    task.valueProperty().addListener((observable, oldValue, newValue) ->
+                            // run this on main thread
+                            Platform.runLater(() -> {
+                                Content content = (Content) newValue;
+                                // if the content is an image, add an ImageView else add a MFXTextField
+                                if (content.getImage() != null) {
+                                    // create hbox with side control buttons
+                                    HBox hBox = addImage(content.getImage());
+                                    // set id that is used for later use
+                                    hBox.setId(String.valueOf(contentMap.get(key)));
+                                    // replace the loading icon with the content on the given index
+                                    children.set(key, hBox);
+                                    new FadeIn(hBox).play();
+                                } else {
+                                    // same as above but use a text instead
+                                    HBox hBox = addText(content.getText());
+                                    hBox.setId(String.valueOf(contentMap.get(key)));
+                                    children.set(key, hBox);
+                                    new FadeIn(hBox).play();
+                                }
+                            }));
+                    es.submit(task);
+                }
+                // shutdown the executor service used for retrieving the content
+                es.shutdown();
+
+            }));
+            // submit the task after the listener is added
+            executorService.submit(mapTask);
+            // shutdown the executor service used for submitting the task
+            executorService.shutdown();
+
+            // set a 30seconds timeout for the executor service
+            try {
+                if (executorService.awaitTermination(30, TimeUnit.SECONDS)) {
+                    executorService.shutdownNow();
+                }
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            // remove the base loading icon
+            children.remove(children.size() - 1);
+        });
+
+
+    }
+
+    private HBox addText(String newValue) {
+        MFXTextField mfxTextField = new MFXTextField();
+        mfxTextField.setFloatMode(FloatMode.BORDER);
+        mfxTextField.setText(newValue);
+        mfxTextField.setMinWidth(scaleReferencePane.getWidth() - scaleOffset);
+        mfxTextField.setMaxWidth(scaleReferencePane.getWidth() - scaleOffset);
+        scaleReferencePane.widthProperty().addListener((o, oldV, newV) -> {
+            mfxTextField.setMinWidth(newV.doubleValue() - scaleOffset);
+            mfxTextField.setMaxWidth(newV.doubleValue() - scaleOffset);
+        });
+        return getHBoxWithNavButtons(mfxTextField);
+    }
+
+    private HBox addImage(Image image) {
+        ImageView imageView = new ImageView();
+
+        imageView.setImage(image);
+        imageView.preserveRatioProperty().set(true);
+        imageView.setFitWidth(scaleReferencePane.getWidth() - scaleOffset);
+        scaleReferencePane.widthProperty().addListener((o, oldV, newV) -> imageView.setFitWidth((double) newV - scaleOffset));
+        return getHBoxWithNavButtons(imageView);
+    }
+
+
+    private HBox getHBoxWithNavButtons(Node node) {
+        HBox hBox = new HBox(8, node, createSideButtonsVBox());
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        return hBox;
+    }
 }
